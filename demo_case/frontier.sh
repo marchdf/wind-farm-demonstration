5f80a94cb6e7fd2ff604d33a8fd240f6d57eba36
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-02-13 15:23:06 -0500

    update submit case and submod

--- demo_case/frontier.sh
+++ demo_case/frontier.sh
@@ -2,9 +2,10 @@
 #SBATCH -J demo_case
 #SBATCH -o %x.o%j
 #SBATCH -A CFD162
-#SBATCH -t 30
-#SBATCH -q debug
-#SBATCH -N 1
+#SBATCH -t 12:00:00
+##SBATCH -t 30
+##SBATCH -q debug
+#SBATCH -N 184
 #SBATCH -S 0
 
 set -e
@@ -25,4 +26,4 @@ cmd "source ${SPACK_MANAGER}/start.sh && spack-start"
 cmd "spack env activate -d ${SPACK_MANAGER}/environments/amr-wind-of"
 cmd "spack load amr-wind"
 cmd "which amr_wind"
-cmd "srun -N1 -n8 -c1 --gpus-per-node=8 --gpu-bind=closest amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 time.max_step=129999"
+cmd "srun -N184 -n1472 -c1 --gpus-per-node=8 --gpu-bind=closest amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 time.max_step=70000"
