bc750db0137e0a36cae14c0495f291ada5442e4f
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-04-26 22:31:05 -0400

    tweaks to run files

--- demo_case/frontier-rst.sh
+++ demo_case/frontier-rst.sh
@@ -3,16 +3,22 @@
 #SBATCH -o %x.o%j
 #SBATCH -A CFD162
 #SBATCH -t 12:00:00
-##SBATCH -t 30
-##SBATCH -q debug
 #SBATCH -N 184
 #SBATCH -S 0
+#SBATCH -d afterany:1709550
 
 set -e
 cmd() {
   echo "+ $@"
   eval "$@"
 }
+
+ofile="demo_case_rst05.inp"
+rfile="demo_case_rst06.inp"
+
+cmd "Xvfb :123 & export DISPLAY=:123"
+cmd "source /ccs/proj/cfd162/lcheung/condaenv/frontier1/bin/activate"
+cmd "python3 ../submods/amr-wind-frontend/utilities/restartAMRWind.py -o ${rfile} ${ofile}"
 
 cmd "module unload PrgEnv-cray"
 cmd "module load PrgEnv-amd"
@@ -26,4 +32,4 @@ cmd "source ${SPACK_MANAGER}/start.sh && spack-start"
 cmd "spack env activate -d ${SPACK_MANAGER}/environments/amr-wind-of-debug"
 cmd "spack load amr-wind"
 cmd "which amr_wind"
-cmd "srun -N184 -n1472 -c1 --gpus-per-node=8 --gpu-bind=closest amr_wind demo_case.inp amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0 time.max_step=57000"
+cmd "srun -N184 -n1472 -c1 --gpus-per-node=8 --gpu-bind=closest amr_wind ${rfile} amrex.abort_on_out_of_gpu_memory=1 amrex.the_arena_is_managed=0 amr.blocking_factor=16 amr.max_grid_size=128 amrex.use_profiler_syncs=0 amrex.async_out=0"
